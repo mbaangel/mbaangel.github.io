47887630fce55e15d4e8375c7e5556b0c0632470
mbaangel <95703191+mbaangel@users.noreply.github.com>
2021-12-11 22:54:49 -0600

    Cuarta actualización

--- catalog/language/ru-ru/common/cart.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Text
-$_['text_items']     = '%s <b>%s</b>';
-$_['text_empty']     = 'Ваша корзина пуста!';
-$_['text_cart']      = 'Корзина';
-$_['text_checkout']  = 'Заказ';
-$_['text_recurring'] = 'Платежный профиль';
-
